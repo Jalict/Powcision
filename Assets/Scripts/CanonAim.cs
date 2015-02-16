47a05a78705cd744d566b433970b24f5814a020d
Frans Larsen <larsen.frans@gmail.com>
2015-02-17 00:08:03 +0100

    The Basics!

---
+++ Assets/Scripts/CanonAim.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanonAim : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// PC Controls
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Rotate(Vector3.back);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Rotate(Vector3.forward);
+		}
+	}
+}
