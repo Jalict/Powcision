de2b2be7c451646d6cd66dc2d42c52c60222b464
Frans Larsen <larsen.frans@gmail.com>
2015-02-17 22:40:07 +0100

    Cleanup

--- Assets/Scripts/CanonAim.cs
+++ Assets/Scripts/CanonAim.cs
@@ -13,25 +13,21 @@ public class CanonAim : MonoBehaviour {
 		if (Input.GetKey (KeyCode.A)) 
         {
             // None of these Vectors makes sense, why? Rotated in Blender?
-            // TODO Take deltaTime into account
 			transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime);
 		}
 		if (Input.GetKey (KeyCode.D)) 
         {
             // None of these Vectors makes sense, why? Rotated in Blender?
-            // TODO Take deltaTime into account
             transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime); 
 		}
         if (Input.GetKey(KeyCode.W))
         {
             // None of these Vectors makes sense, why? Rotated in Blender?
-            // TODO Take deltaTime into account
             transform.FindChild("Canon-barrel").Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.S))
         {
             // None of these Vectors makes sense, why? Rotated in Blender?
-            // TODO Take deltaTime into account
             transform.FindChild("Canon-barrel").Rotate(Vector3.left * rotationSpeed * Time.deltaTime);
         }
 	}
