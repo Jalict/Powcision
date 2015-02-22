77d40f7cf8b755aeb481b0c781984503ae6e1fc9
Frans Larsen <larsen.frans@gmail.com>
2015-02-22 20:45:53 +0100

    Initial Destruction and target script.

---
+++ Assets/Scripts/Targets.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Targets : MonoBehaviour {
+	[Header("Prop Information")]
+	public string name;
+	public int health;
+
+	[Header("Destruction")]
+	public GameObject destructionPrefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		// If object have lost all health, then replace object with destroyed one.
+		if (health <= 0) {
+			Instantiate(destructionPrefab,transform.position,transform.rotation);
+
+			Destroy(gameObject);
+		}
+	}
+}
