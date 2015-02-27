c4d53637d5cc47a7156a081a5acc5581b67a4561
Frans Peter Larsen <larsen.frans@gmail.com>
2015-02-27 11:24:20 +0100

    Script to default rotation of models

---
+++ Assets/Scripts/RotationVectors.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationVectors : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.forward);
+        Gizmos.color = Color.green;
+        Gizmos.DrawRay(transform.position, transform.up);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(transform.position, transform.right);
+    }
+}
