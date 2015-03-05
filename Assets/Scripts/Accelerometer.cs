baaae3ea68ec2ec3ceb1fdf5e2d395ab133434d2
Frans Peter Larsen <larsen.frans@gmail.com>
2015-03-05 09:20:24 +0100

    Accelerometer testing

---
+++ Assets/Scripts/Accelerometer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Accelerometer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //TODO Look into this
+        transform.rotation = Quaternion.LookRotation(Input.acceleration, Vector3.right);
+	}
+}
